faacf39dcd42a87ca1f050b2239184113d435553
Grigore Vieru <vieru.grigore896@gmail.com>
2021-11-24 18:35:01 +0200

    implemented footer section

---
+++ index.js
@@ -0,0 +1,13 @@
+// function underlineNavLink() {
+//     console.log("start")
+//     let activeLink = document.getElementsByClassName("menu-link-active");
+//     console.log(activeLink);
+//     if (activeLink.length > 0) {
+//         activeLink.classList.remove("menu-link-active");
+//     }
+//     window.onclick = event => {
+//
+//         const linkToActivate = event.target;
+//         linkToActivate.classList.add("menu-link-active");
+//     }
+// }
